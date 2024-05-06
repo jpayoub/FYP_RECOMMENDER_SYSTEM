303db10a4b03da9f36cf313854603bd36369141d
JP Ayoub <jeanpierre.aub@hotmail.com>
2024-05-07 02:27:49 +0300

    adjusting home page

--- src/screens/Result.tsx
+++ src/screens/Result.tsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LottieView from "lottie-react-native";
 import lottieAnimation from '../assets/lottie/Animation-ai.json';
-import { View } from "react-native";
-import { createEntityAdapter } from "@reduxjs/toolkit";
-
+import { View, Text } from "react-native";
+import CustomButton from "../components/atoms/CustomButton";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Animation() {
+  const [animationFinished, setAnimationFinished] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Set animationFinished to true after 3 seconds
+      setAnimationFinished(true);
+    }, 2500); // 3000 milliseconds = 3 seconds
+
+    return () => clearTimeout(timer);
+  }, []); 
+
+  const navigation = useNavigation();
+
+  const navigatehome = () => {
+    navigation.navigate("Home");
+  }
+
   return (
-    <View style={{flex:1, justifycontent:'center', alignItems: 'center' }}>
-    <LottieView
-      source={require('../assets/lottie/Animation-ai.json')}
-      style={{width: '100%', height:'100%'}}
-      autoPlay
-      loop
-    />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {!animationFinished ? (
+        <LottieView
+          source={require('../assets/lottie/Animation-ai.json')}
+          style={{ width: '100%', height: '100%' }}
+          autoPlay
+          loop={false} 
+          onAnimationFinish={() => setAnimationFinished(true)} // Update state when animation finishes
+        />
+      ) : (
+        <View>
+          <Text>Hello result</Text>
+
+          <CustomButton 
+          onPress={navigatehome} 
+          text="Go Back Home"
+          type="PRIMARY" />
+        </View>
+      )}
     </View>
   );
 }
